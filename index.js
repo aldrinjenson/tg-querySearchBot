bbc189bcc323132c3f54476cc6e5d464b88f860a
Aldrin Jenson <aldrinjenson@gmail.com>
2021-06-27 21:38:15 +0530

    Added query search

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 process.env.NTBA_FIX_319 = 1;
 const TelegramBot = require("node-telegram-bot-api");
-const axios = require("axios");
+const bing = require("bing-scraper");
 
 const bot = new TelegramBot(process.env.BOT_TOKEN, { polling: true });
 bot.on("polling_error", console.log);
@@ -18,19 +18,46 @@ bot.onText(/\/start/, (msg) => {
 //     chatId,
 //     "Inavlid command format\nPlease enter /play song name\neg: /play hello adele"
 //   );
-// });
+// })
 
-bot.onText(/\/qs/, (msg, match) => {
+bot.addListener('inline_query',query=>{
+  console.log(query)
+})
+
+const getResults = (query) => {
+  return new Promise((resolve, reject) => {
+    bing.search(
+      {
+        q: query,
+        enforceLanguage: true,
+      },
+      (err, resp) => {
+        if (err) {
+          reject(err);
+        } else {
+          const { results } = resp;
+          const entries = results.map(item=>{
+            const { title, description, url } = item;
+            const abstract = description.substring(0, 250) + "...";
+            return `${title}\n${abstract}\n${url}\n\n`;
+          })
+          resolve(entries)
+        }
+      }
+    );
+  });
+};
+
+const MAX_RESULTS = 3
+
+bot.onText(/\/qs (.+)/, (msg, match) => {
   const chatId = msg.chat.id;
-  // const query =  match[1];
-  const query = "largest animal in the world";
-  axios
-    .get(
-      `https://api.wolframalpha.com/v1/result?i=${query}%3F&appid=${process.env.APP_ID}`
-    )
-    .then((res) => {
-      bot.sendMessage(res.data);
-      console.log(res.data);
-    })
-    .catch((err) => console.log(err));
+  const query = match[1] || "largest animal in the world";
+  bot.sendMessage(chatId, "Searching..");
+
+  getResults(query).then((results) => {
+    for(let i = 0; i < MAX_RESULTS; i ++){
+      bot.sendMessage(chatId, results[i]);
+    }
+  });
 });
